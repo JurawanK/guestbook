0a8965eac72dc92c8bd3423f6f47c549e82415aa
NICKNAN <jurawankp@gmail.com>
2023-01-09 12:08:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form action= "slow.php">
+        What is your name:<input type="text" name="name"><br>
+        What is your age:<input type="text" name="age"><br>
+        What is your gender:<input type="text" name="gender"><br>
+        
+        <input type="submit" value="submit">
+    <form>    
+
+
+</body>
+</html>
