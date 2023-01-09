894ddefd1dc43a9d8e901a8b2f4afb4a14303315
NICKNAN <jurawankp@gmail.com>
2023-01-09 12:17:09 +0700

    changes status

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         What is your name:<input type="text" name="name"><br>
         What is your age:<input type="text" name="age"><br>
         What is your gender:<input type="text" name="gender"><br>
-        
+        What is your status:<input type="text" name="status"><br>
         <input type="submit" value="submit">
     <form>    
 
