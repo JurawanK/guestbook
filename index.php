e52a33b4c91af6c735d3cf2a66b364e7e34a614a
JurawanK <jurawan.k@ku.th>
2023-01-15 20:48:34 +0700

    7

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
         What is your age: <input type = "text" name = "age"> <br>
         What is your gender: <br>
         <input type = "radio" name = "gender" value = "ชาย"> ชาย <br>
-        <input type = "radio" name = "gender" value = "female"> หญิง <br>
+        <input type = "radio" name = "gender" value = "หญิง"> หญิง <br>
         What is your marrital status: <br>
-        <input type = "radio" name = "status" value = "single"> โสด <br>
-        <input type = "radio" name = "status" value = "married"> สมรส <br>
-        <input type = "radio" name = "status" value = "divorced"> หย่าร้าง <br>
+        <input type = "radio" name = "status" value = "โสด"> โสด <br>
+        <input type = "radio" name = "status" value = "สมรส"> สมรส <br>
+        <input type = "radio" name = "status" value = "หย่าร้าง"> หย่าร้าง <br>
         <input type = "submit" value = "sumit">       
     </form>
     
