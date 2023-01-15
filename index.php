687e9820dcefc93988ae76026c05b1c3091037e3
JurawanK <jurawan.k@ku.th>
2023-01-15 20:58:15 +0700

    9

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 <body>
 
     <form action="show.php">
-        What is your name: <input type = "text" name = "name"> <br>
-        What is your age: <input type = "text" name = "age"> <br>
-        What is your gender: <br>
+        ชื่อ-นามสกุล : <input type = "text" name = "name"> <br>
+        อายุ : <input type = "text" name = "age"> <br>
+        เพศ : <br>
         <input type = "radio" name = "gender" value = "ชาย"> ชาย <br>
         <input type = "radio" name = "gender" value = "หญิง"> หญิง <br>
-        What is your marrital status: <br>
+        สถานะ : <br>
         <input type = "radio" name = "status" value = "โสด"> โสด <br>
         <input type = "radio" name = "status" value = "สมรส"> สมรส <br>
         <input type = "radio" name = "status" value = "หย่าร้าง"> หย่าร้าง <br>
